11700b047e2345c4fe422d34026cdb1eede5bdd7
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2018-02-17 15:02:28 -0200

    Created population combo profile

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,22 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {UserProvider} from "../../providers/user/user";
+import {ProfileProvider} from "../../providers/profile/profile";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
   providers: [
-    UserProvider
+    UserProvider,
+    ProfileProvider
   ]
 })
 export class HomePage {
 
   public users = [];
+  public profiles = [];
 
-  public user = {"_id":"", "name":"", "email":""};
+  public user = {"_id":"", "name":"", "email":"", "profile":""};
 
-  constructor(public navCtrl: NavController, private userService:UserProvider) {
+  constructor(public navCtrl: NavController, private userService:UserProvider, private profileService:ProfileProvider) {
     this.getUsers();
+    this.getProfiles();
   }
 
   public getUsers() {
@@ -34,11 +38,19 @@ export class HomePage {
       this.userService.update(this.user).subscribe(respose => this.getUsers());
     }
 
-    this.user = {"_id":"", "name":"", "email":""};
+    this.user = {"_id":"", "name":"", "email":"", "profile":""};
   }
 
   public deleteUser(id) {
     this.userService.delete(id).subscribe(respose => this.getUsers());
+  }
+
+  public compareFn(e1: any, e2: any): boolean {
+    return e1 && e2 ? e1.id === e2.id : e1 === e2;
+  }
+
+  public getProfiles() {
+    this.profileService.findAll().subscribe(response => this.profiles = response);
   }
 
 }
